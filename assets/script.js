0a51e2ce209becb3a61278ee0bc69e877c520f2f
Dario Gomez <dariogomezaguirre@gmail.com>
2021-06-20 18:49:46 -0500

    fixed fetch and submit button

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,29 @@
 var cityNameSearch = document.querySelector("#city-name");
 var userCitySearch = document.querySelector("#city");
+var searchButton = document.querySelector("#search-button");
+var formSearch = document.querySelector("#search-city");
 
-var getWeatherRepo = function () {
+var getWeatherRepo = function (user) {
   fetch(
     "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=ba16556d951a6c55d84f1ab8d3febc95"
-  );
+  ).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 };
 
 getWeatherRepo();
 
-var cityNameSearchBar = function (event) {
-  event.preventDefault();
-  console.log(event);
+var displayWeatherRepo = function (repos, searchTerm) {
+  console.log(repos);
+  console.log(searchTerm);
 };
 
-userCitySearch.addEventListener("submit", cityNameSearch);
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var 
+};
+
+formSearch.addEventListener("submit", formSubmitHandler);
