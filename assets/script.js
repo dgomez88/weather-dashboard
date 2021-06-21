ee7784641e7ec2cbf925c4e133edfe4ef1085c8d
Dario Gomez <dariogomezaguirre@gmail.com>
2021-06-20 20:05:55 -0500

    updated functions

--- assets/script.js
+++ assets/script.js
@@ -2,28 +2,44 @@ var cityNameSearch = document.querySelector("#city-name");
 var userCitySearch = document.querySelector("#city");
 var searchButton = document.querySelector("#search-button");
 var formSearch = document.querySelector("#search-city");
+var cityContainer = document.querySelector("#city-info-container");
+var cityInfoSearch = document.querySelector("#city-info-search");
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var username = cityNameSearch.value.trim();
+
+  if (username) {
+    getWeatherRepo(username);
+    cityNameSearch.value = "";
+  } else {
+    alert("Please enter a city name");
+  }
+};
+
+var displayWeatherRepo = function (displayRepo, searchTerm) {
+  for (var i = 0; i < displayRepo.length; i++) {
+    var displayRepoName =
+      displayRepo[i].displayWeatherRepo.login + "/" + displayRepo[i].name;
+
+    var displayWeatherEl = document.createElement("span");
+
+    var displayTitleEl = document.createElement();
+
+    displayWeatherEl.appendChild();
+  }
+};
 
 var getWeatherRepo = function (user) {
   fetch(
     "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=ba16556d951a6c55d84f1ab8d3febc95"
   ).then(function (response) {
     response.json().then(function (data) {
-      console.log(data);
+      console.log(data, user);
     });
   });
 };
 
 getWeatherRepo();
-
-var displayWeatherRepo = function (repos, searchTerm) {
-  console.log(repos);
-  console.log(searchTerm);
-};
-
-var formSubmitHandler = function (event) {
-  event.preventDefault();
-
-  var 
-};
-
 formSearch.addEventListener("submit", formSubmitHandler);
